daa1a93638602ab0e667eb13fc3cbce23ab58d84
Joshua Poddoku <201751033@iiitvadodara.ac.in>
2020-03-14 13:20:37 +0530

    added header component with logos

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,6 +1,58 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import RMDBLogo from '../images/reactMovie_logo.png';
+import TMDBLogo from '../images/tmdb_logo.svg';
 
 
-const Header = () => <div>Header</div>      //implicit return in arrow function
+const StyledHeader = styled.div`
+background: #353F48;
+padding: 0px 20px;
+box-sizing: border-box;
+
+.header-content{
+    max-width: 1280px;
+    min-height: 120px;
+    padding: 20px 0px;
+    margin: 0 auto;
+    box-sizing: border-box;
+
+    @media screen and (max-width: 500px){
+        min-height: 500px;
+    }
+}
+`;
+
+const StyledRMDBLogo = styled.img`
+    width: 250px;
+    margin-top: 20px;
+
+    @media screen and (max-width: 500px){
+        width: 150px;
+        margin-top: 5px;
+
+    }
+
+`;
+
+const StyledTMDBLogo = styled.img`
+    width: 122px;
+    margin-top: 25px;
+    float: right;
+
+    @media screen and (max-width: 500px){
+        display: inline-block;
+        width: 80px;
+        margin-top: 0px;
+    }
+`;
+const Header = () => (
+<StyledHeader>
+    <div className="header-content">
+        <StyledRMDBLogo src={RMDBLogo} alt="rmdb-logo" />
+        <StyledTMDBLogo src={TMDBLogo} alt="rmdb-logo" />
+    </div>
+</StyledHeader>
+)      //implicit return in arrow function
 
 export default Header;
